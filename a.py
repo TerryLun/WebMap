18003756b958a54a1f3c0399499c61e110384bc3
TerryLun <tianweilun@yahoo.com>
2020-02-01 21:54:23 -0800

    added FeatureGroup

--- a.py
+++ a.py
@@ -3,8 +3,10 @@ import folium
 # initialize map
 map1 = folium.Map(location=[49.189848, -123.126379], zoom_start=13, tiles="Stamen Terrain")
 
-# add marker
-map1.add_child(folium.Marker(location=[49.189848, -123.126379], popup='Home', icon=folium.Icon(color='green')))
+# add marker using FeatureGroup
+fg = folium.FeatureGroup(name='My Map')
+fg.add_child(folium.Marker(location=[49.189848, -123.126379], popup='Home', icon=folium.Icon(color='green')))
+map1.add_child(fg)
 
 # generate map
 map1.save('map1.html')
