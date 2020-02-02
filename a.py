8084a10d6ea05f93aa47417fcc185b7f43574d28
TerryLun <tianweilun@yahoo.com>
2020-02-01 22:01:15 -0800

    changed naming

--- a.py
+++ a.py
@@ -1,12 +1,12 @@
 import folium
 
 # initialize map
-map1 = folium.Map(location=[49.189848, -123.126379], zoom_start=13, tiles="Stamen Terrain")
+richmond_map = folium.Map(location=[49.189848, -123.126379], zoom_start=13, tiles="Stamen Terrain")
 
-# add marker using FeatureGroup
+# add markers using FeatureGroup
 fg = folium.FeatureGroup(name='My Map')
 fg.add_child(folium.Marker(location=[49.189848, -123.126379], popup='Home', icon=folium.Icon(color='green')))
-map1.add_child(fg)
+richmond_map.add_child(fg)
 
 # generate map
-map1.save('map1.html')
+richmond_map.save('richmond_map.html')
