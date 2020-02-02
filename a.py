7e0609923672cb4a6047640bfebadac2c31a8b93
TerryLun <tianweilun@yahoo.com>
2020-02-01 21:30:15 -0800

    new tiles

--- a.py
+++ a.py
@@ -1,7 +1,7 @@
 import folium
+# tiles = "Stamen Terrain"
 
-
-map1 = folium.Map(location=[49.189848, -123.126379], zoom_start=11)
+map1 = folium.Map(location=[49.189848, -123.126379], zoom_start=11, tiles="Stamen Terrain")
 
 
 map1.save('map1.html')
