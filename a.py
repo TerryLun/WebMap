257c152c3e536eef894fdb8e3a52777821c4c7c0
TerryLun <tianweilun@yahoo.com>
2020-02-01 21:50:41 -0800

    add home marker

--- a.py
+++ a.py
@@ -1,7 +1,10 @@
 import folium
-# tiles = "Stamen Terrain"
 
-map1 = folium.Map(location=[49.189848, -123.126379], zoom_start=11, tiles="Stamen Terrain")
+# initialize map
+map1 = folium.Map(location=[49.189848, -123.126379], zoom_start=13, tiles="Stamen Terrain")
 
+# add marker
+map1.add_child(folium.Marker(location=[49.189848, -123.126379], popup='Home', icon=folium.Icon(color='green')))
 
+# generate map
 map1.save('map1.html')
