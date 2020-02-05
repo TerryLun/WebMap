f4ba83a0c3cb1ad1e13c5a085b7f2518be383dd7
TerryLun <tianweilun@yahoo.com>
2020-02-04 21:54:59 -0800

    color markers by elevation

--- webmap.py
+++ webmap.py
@@ -2,7 +2,7 @@ import folium
 import pandas
 
 # initialize map
-volcanoes_map = folium.Map(location=[38.58,-99.09], zoom_start=5, tiles="Stamen Terrain")
+volcanoes_map = folium.Map(location=[38.58, -99.09], zoom_start=5, tiles="Stamen Terrain")
 
 data = pandas.read_csv('volcanoes.txt')
 lat = list(data['LAT'])
@@ -15,11 +15,22 @@ html = """<h4>Volcano information:</h4>
 Height: %s m
 """
 
+
+def color_producer(ele):
+    if ele < 1000:
+        return 'green'
+    elif 1000 <= ele < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
+
 # add markers using FeatureGroup
 fg = folium.FeatureGroup(name='My Map')
 for lt, ln, el, nm in zip(lat, lon, elev, name):
     iframe = folium.IFrame(html=html % (nm, nm, el), width=200, height=100)
-    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color='red')))
+    fg.add_child(
+        folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color=color_producer(el))))
 volcanoes_map.add_child(fg)
 
 # generate map
