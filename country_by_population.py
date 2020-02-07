d7bc781fb1efa4f52c6cd38f61a955bfccc36641
TerryLun <tianweilun@yahoo.com>
2020-02-06 22:53:51 -0800

    added layer control

--- country_by_population.py
+++ country_by_population.py
@@ -2,15 +2,21 @@ import folium
 import pandas
 
 # initialize map
-volcanoes_map = folium.Map(location=[34.3946, 147.2062], zoom_start=3, tiles="Stamen Terrain")
+country_by_population_map = folium.Map(location=[41.4432, 2.0803], zoom_start=3, tiles="Stamen Terrain")
 
 # FeatureGroup 'My Map'
-fg = folium.FeatureGroup(name='My Map')
+fg = folium.FeatureGroup(name='Population by Colour')
 
-fg.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='UTF-8-sig').read())))
+# read Geojson file then add child
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='UTF-8-sig').read(), style_function=lambda x: {
+    'fillColor': 'green' if x['properties']['POP2005'] < 10_000_000 else 'orange' if 10_000_000 <= x['properties'][
+        'POP2005'] < 20_000_000 else 'red'}))
 
 # add feature group
-volcanoes_map.add_child(fg)
+country_by_population_map.add_child(fg)
+
+# add layer control
+country_by_population_map.add_child(folium.LayerControl())
 
 # generate map
-volcanoes_map.save('country_by_population.html')
+country_by_population_map.save('country_by_population.html')
